510b36264549d0dc8bd044f0cf23d804147fce08
Timofey <timofejfomin99@gmail.com>
2026-03-01 01:27:18 +0300

    All basic functions

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from kivy.uix.textinput import TextInput
 from pydantic import BaseModel, Field, ValidationError
 from typing import Optional
 import ruffier
-
+from kivy.properties import BooleanProperty
+from kivy.clock import Clock
 
 class MyApp(App):
     def build(self):
@@ -51,65 +52,112 @@ class SecondScreen(Screen):
         layout = BoxLayout(orientation='vertical', spacing='10px', padding='10px')
         label = Label(text=instructions.txt_test1)
         self.textinput = TextInput(size_hint=(0.8, None), height='40px',
-                                   pos_hint={'center_x': 0.5}, hint_text='Введите результат')
-        button = Button(text='Продолжить', background_color='green',
+                                   pos_hint={'center_x': 0.5}, hint_text='Введите результат',disabled=True)
+        self.button = Button(text='Нажмите, чтобы запустить таймер', background_color='green',
                         size_hint=(0.8, None), height='40px', pos_hint={'center_x': 0.5})
-
+        self.timer = Timer(15)
+        self.timer.bind(is_finish=self.unlock_items)
         layout.add_widget(label)
+        layout.add_widget(self.timer)
         layout.add_widget(self.textinput)
-        layout.add_widget(button)
+        layout.add_widget(self.button)
         self.add_widget(layout)
-        button.on_press = self.changing_screen
+        self.button.on_press = self.changing_screen
     def changing_screen(self):
+        if not self.timer.is_finish:
+            self.button.disabled = True
+            self.button.text = 'Продолжить'
+            self.timer.start_timer()
         try:
             app.p1 = SchemaDimension(dimension=self.textinput.text)
             self.manager.current = 'third screen'
         except ValidationError:
             self.textinput.text = ''
+    def unlock_items(self, *args):
+        self.button.disabled = False
+        self.textinput.disabled = False
 class ThirdScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         layout = BoxLayout(orientation='vertical', spacing='10px', padding='10px')
         label = Label(text=instructions.txt_sits)
-        button = Button(text='Продолжить', background_color='green',
+        self.button = Button(text='Запустить таймер', background_color='green',
                         size_hint=(0.8, None), height='40px', pos_hint={'center_x': 0.5})
+        self.timer = Timer(45)
+        self.timer.bind(is_finish=self.unlock_button)
         layout.add_widget(label)
-        layout.add_widget(button)
+        layout.add_widget(self.timer)
+        layout.add_widget(self.button)
         self.add_widget(layout)
-        button.on_press = self.changing_screen
+        self.button.on_press = self.changing_screen
     def changing_screen(self):
-        self.manager.current = 'fourth screen'
+        if not self.timer.is_finish:
+            self.button.disabled = True
+            self.timer.start_timer()
+        if self.button.text == 'Продолжить':
+            self.manager.current = 'fourth screen'
+    def unlock_button(self, *args):
+        self.button.text = 'Продолжить'
+        self.button.disabled = False
+
 class FourthScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         layout = BoxLayout(orientation='vertical', spacing='10px', padding='10px')
         label = Label(text=instructions.txt_test3)
         self.textinput_res = TextInput(size_hint=(0.8, None), height='40px',
-                                   pos_hint={'center_x': 0.5}, hint_text='Результат')
+                                   pos_hint={'center_x': 0.5}, hint_text='Результат',
+                                   disabled = True)
         self.textinput_res2 = TextInput(size_hint=(0.8, None), height='40px', pos_hint={'center_x': 0.5},
-                                  hint_text='Результат после отдыха')
-        button = Button(text='Завершить', background_color='green',
+                                  hint_text='Результат после отдыха', disabled = True)
+        self.button = Button(text='Запустить таймер', background_color='green',
                         size_hint=(0.8, None), height='40px', pos_hint={'center_x': 0.5})
+        self.stages = [15, 30, 15]
+        self.stages_index = 0
+        self.timer = Timer(self.stages[self.stages_index])
+        self.timer.bind(is_finish=self.restarting_timer)
         layout.add_widget(label)
+        layout.add_widget(self.timer)
         layout.add_widget(self.textinput_res)
         layout.add_widget(self.textinput_res2)
-        layout.add_widget(button)
+        layout.add_widget(self.button)
         self.add_widget(layout)
-        button.on_press = self.changing_screen
+        self.button.on_press = self.changing_screen
     def changing_screen(self):
-        try:
-            app.p2 = SchemaDimension(dimension=self.textinput_res.text)
-        except ValidationError:
-            self.textinput_res.text = ''
-        try:
-            app.p3 = SchemaDimension(dimension=self.textinput_res2.text)
-            self.manager.current = 'fifth screen'
-        except ValidationError:
-            self.textinput_res2.text = ''
+        if not self.timer.is_finish:
+            self.button.disabled = True
+            self.timer.start_timer()
+        elif self.button.text == 'Завершить':
+            try:
+                app.p2 = SchemaDimension(dimension=self.textinput_res.text)
+            except ValidationError:
+                self.textinput_res.text = ''
+            try:
+                app.p3 = SchemaDimension(dimension=self.textinput_res2.text)
+                self.manager.current = 'fifth screen'
+            except ValidationError:
+                self.textinput_res2.text = ''
+
+    def restarting_timer(self, instance, value, ):
+        if value is not True:
+            return
+
+        self.stages_index += 1
+        if self.stages_index < len(self.stages):
+            if self.stages_index == 1:
+                self.textinput_res.disabled = False
+            elif self.stages_index == 2:
+                self.textinput_res2.disabled = False
+            self.timer.is_finish = False
+            self.timer.restart_timer(self.stages[self.stages_index])
+        else:
+            self.button.text = 'Завершить'
+            self.button.disabled = False
+
 class FifthScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.label = Label(text='123')
+        self.label = Label(text='')
         self.add_widget(self.label)
         self.on_enter = self.get_answer
     def get_answer(self):
@@ -120,6 +168,32 @@ class SchemaFirstScreen(BaseModel):
     name: Optional[str]
 class SchemaDimension(BaseModel):
     dimension: Optional[int] = Field(..., ge=1)
+class Timer(Label):
+    is_finish = BooleanProperty(False)
+    def __init__(self, time, **kwargs):
+        self.time = time
+        self.is_finish = False
+        self.current = 0
+        self.text = f'Прошло секунд: {self.current}'
+        self.event = None
+        super().__init__(text=self.text, **kwargs)
+    def change_text_timer(self, dt):
+        self.current += 1
+        self.text = f'Прошло секунд: {self.current}'
+        if self.current >= self.time:
+            self.is_finish = True
+            return False
+    def restart_timer(self, time):
+        self.is_finish = False
+        self.time = time
+        self.current = 0
+        self.text = f'Прошло секунд: {self.current}'
+        self.start_timer()
+    def start_timer(self):
+        Clock.schedule_interval(self.change_text_timer, 1)
+
+
+
 
 
 
